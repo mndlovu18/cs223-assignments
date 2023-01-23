53713b0e45a75ac7176e70d9baef5f399340ea50
Minolta Ndlovu <mndlovu@goldengate.cs.brynmawr.edu>
2023-01-23 18:06:45 -0500

    Submitting Assignment 01

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,40 @@
 /***************************************************
- * mathgame.c
- * Author: 
+* mathgame.c
+ * Author:Minolta Ndlovu 
  * Implements a math game
  */
 
 #include <stdio.h>
 
 int main() {
-  return 0;
+    printf("Welcome to Math Game!/n");
+    printf("How many rounds do you want to play? ");
+
+    int rounds;
+    scanf("%d/n", &rounds);
+
+    int correct = 0;
+
+    for (int i = 0; i < rounds; i++) {
+        int a = rand() % 9 + 1;
+        int b = rand() % 9 + 1;
+        int answer = a + b;
+
+        printf("%d + %d = ? ", a, b);
+
+        int user_answer;
+        scanf("%d", &user_answer);
+
+        if (user_answer == answer) {
+            printf("Correct!");
+            correct++;
+        } else {
+            printf("Incorrect :(");
+        }
+
+    }
+
+    printf("You answered %d/%d correctly.", correct, rounds);
+
 }
+
