3ef9a44ba4e0f334fd73b0d552a9f8642f079d83
Minolta Ndlovu <mndlovu@goldengate.cs.brynmawr.edu>
2023-02-08 21:29:44 -0500

    Submit assignment 3

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,10 +1,90 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author:Minolta Ndlovu 
+ * Date: 02/07/2023
+ * Description: A program thats reads text songs 
+   into an array.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-  return 0;
+
+struct song {  // a struct song to hold the song information.
+    char title[128];
+    char artist[128];
+    int duration;
+    float danceability;
+    float energy;
+    float tempo;
+    float valence;
+};
+
+/**
+* Prints a table of songs. 
+* @param songs The array of songs
+* @param num_songs The number of songs in the array
+* @return void
+*/
+void print_songs(struct song *songs, int num_songs) {
+    printf("Welcome to Dynamic Donna's Danceability Directory. \n");
+    printf("\n");
+    for (int i = 0; i < num_songs; i++) {
+        printf("%d) %-30s artist: %-20s duration: %d:%02d D: %0.3f E: %0.3f T: %0.3f V: %0.3f \n", i, songs[i].title, songs[i].artist, songs[i].duration / 60000, (songs[i].duration % 60000) / 1000, songs[i].danceability, songs[i].energy, songs[i].tempo, songs[i].valence);
+
+    }
+}
+
+/**
+ * Reads a CSV file of songs and prints them out.
+ * @return int 
+ */
+
+int main(void) {
+    FILE *songs = fopen("songlist.csv", "r"); // Open the file for reading.
+    if (songs == NULL) {
+        printf("Error opening file.");
+        return 1;
+    }
+    
+    char line[128]; // Read the number of songs from the first line.
+    fgets(line, 128, songs);
+    int num_songs = atoi(strtok(line, ","));
+
+    char header[128]; // Read the header line.
+    fgets(header, 128, songs);
+
+    struct song *song_list = malloc(num_songs * sizeof(struct song)); // Allocate an array of songs using malloc
+    int j = 0;
+    while (fgets(line, 128, songs) != NULL) {
+        struct song line_song; // Create a song struct for each line.
+        char *token = strtok(line, ",");
+
+        int i = 0;
+        while (token != NULL) {
+            if (i == 0) {
+                strcpy(line_song.title, token);
+            } else if (i == 1) {
+                strcpy(line_song.artist, token);
+            } else if (i == 2) {
+                line_song.duration = atoi(token);
+            } else if (i == 3) {
+                line_song.danceability = atof(token);
+            } else if (i == 4) {
+                line_song.energy = atof(token);
+            } else if (i == 5) {
+                line_song.tempo = atof(token);
+            } else if (i == 6) {
+                line_song.valence = atof(token);
+            }
+            i++;
+            token = strtok(NULL, ","); 
+        }
+
+        song_list[j] = line_song;  // Add the song struct to the array.
+        j++;
+    }
+   print_songs(song_list, num_songs); //print all the songs in a table
+
+   free(song_list); //free storage
+   return 0;
 }
--- A03/slow.c
+++ A03/slow.c
@@ -1,10 +1,49 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author:Minolta Ndlovu 
+ * Date: 02/08/2022
+ * Description: This program takes a string and adds pauses between each letter
  ---------------------------------------------*/
-#include <stdio.h>
 
-int main() {
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* Turns a phrase into ent-speech
+* @return int
+*/
+int main(void) {
+    int pause;
+    char buff[32];
+    char *ent;
+    int i, j, k;
+
+    printf("Pause length: ");
+    scanf("%d", &pause);
+    printf("Text: ");
+    scanf("%s", buff);
+
+    ent = malloc(strlen(buff) * pause + strlen(buff) + 1); //a malloc string that can hold the ent speech with pauses (len(string) * pause + len(string))
+
+
+    for (i = 0, j = 0; i < strlen(buff); i++, j++) {
+        if (i == 0) {
+            ent[j] = buff[i];
+            continue; //if its the first letter, dont add a pause
+        }
+
+        for (k = 0; k < pause; k++, j++) {
+            ent[j] = '.'; //add the pause
+        }
+        ent[j] = buff[i];
+    }
+
+    ent[j] = '\0';
+
+    printf("%s \n", ent);
+
+    free(ent);
+
+    return 0;
+
 }
