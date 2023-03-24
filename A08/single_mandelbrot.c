11b3ed7fa4eccc4790b004cae4e61df5879d4b97
Minolta Ndlovu <62041069+mndlovu18@users.noreply.github.com>
2023-03-24 07:21:32 -0500

    assignment complete

--- A08/single_mandelbrot.c
+++ A08/single_mandelbrot.c
@@ -1,3 +1,10 @@
+/*----------------------------------------------
+ * Author: Minolta Ndlovu
+ * Date: 03/24/2023
+ * Description: This program outputs a PPM image 
+ * of the mandelbrot set
+ ---------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,6 +13,31 @@
 #include "read_ppm.h"
 #include "write_ppm.h"
 
+/*
+  * This function computes the mandelbrot set
+  * @param real: the real part of the complex number
+  * @param imag: the imaginary part of the complex number
+  * @param maxIterations: the maximum number of iterations
+  * @return: the number of iterations
+*/
+int mandelbrot(float real, float imag, int maxIterations) {
+  int n;
+  float r = 0.0, i = 0.0, rr = 0.0, ii = 0.0;
+  for (n = 0; n < maxIterations && rr + ii <= 4.0; n++) {
+    i = 2 * r * i + imag;
+    r = rr - ii + real;
+    rr = r * r;
+    ii = i * i;
+  }
+  return n;
+}
+
+/*
+  * This function writes the image to a PPM file
+  * @param argc: the number of command line arguments
+  * @param argv: the command line arguments
+  * @return: 0 if successful
+*/
 int main(int argc, char* argv[]) {
   int size = 480;
   float xmin = -2.0;
@@ -25,14 +57,44 @@ int main(int argc, char* argv[]) {
       case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> -b <ymin> -t <ymax>\n", argv[0]); break;
     }
   }
+
   printf("Generating mandelbrot with size %dx%d\n", size, size);
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your work here
-  // generate pallet
+  struct timeval start, end;
+  gettimeofday(&start, NULL);
+
+  struct ppm_pixel* image = (struct ppm_pixel*) malloc(size * size * sizeof(struct ppm_pixel));
+  float xScale = (xmax - xmin) / size;
+  float yScale = (ymax - ymin) / size;
+
   srand(time(0));
 
-  // compute image
+  for (int y = 0; y < size; y++) { //for each row
+    for (int x = 0; x < size; x++) { //for each column
+      float real = xmin + x * xScale;
+      float imag = ymax - y * yScale;
+      int value = mandelbrot(real, imag, maxIterations); //compute the mandelbrot set
 
-}
+      image[y * size + x] = (struct ppm_pixel){ //assign the pixel
+        .red = (value * 5) % 256,
+        .green = (value * 10) % 256,
+        .blue = (value * 20) % 256
+      };
+    }
+  }
+
+  gettimeofday(&end, NULL); //end the timer
+  double elapsed = (end.tv_sec - start.tv_sec) + 1e-6 * (end.tv_usec - start.tv_usec); 
+  printf("Computed mandelbrot set (%dx%d) in %f seconds\n", size, size, elapsed);
+
+  char filename[128];
+  snprintf(filename, sizeof(filename), "mandelbrot-%d-%ld.ppm", size, time(0));
+  write_ppm(filename, image, size, size);  //write the image to a PPM file
+  printf("Writing file: %s\n", filename);
+
+  free(image); 
+  return 0; 
+} 
+
--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,51 @@
-#include <stdio.h>
+/*----------------------------------------------
+ * Author: Minolta Ndlovu
+ * Date: 03/24/2023
+ * Description: This program spawns processes A, B, and C
+ ---------------------------------------------*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/*
+ * This function spawns processes A, B, and C
+ * @return: 0 if successful
+*/
 int main() {
-  return 0;
+    pid_t pid_A, pid_B; 
+
+    printf("%d] A0\n", getpid()); 
+
+    pid_A = fork();
+
+    if (pid_A > 0) {
+        printf("%d] BO\n", getpid()); //if parent, print B0
+    }
+
+    if (pid_A == 0) {
+        printf("%d] B1\n", getpid()); //if child, print B1
+        printf("%d] Bye\n", getpid()); //if child, print Bye
+        exit(0);
+    }
+
+    
+    pid_B = fork(); //fork again
+
+
+    if (pid_B > 0) {
+        printf("%d] C0\n", getpid()); //if parent, print C0
+        printf("%d] Bye\n", getpid()); //if parent, print Bye
+        exit(0);
+    }
+
+    if (pid_B == 0) {
+        printf("%d] C1\n", getpid()); //if child, print C1
+        printf("%d] Bye\n", getpid()); //if child, print Bye
+        exit(0);
+    }
+
+    return 0;
 }
